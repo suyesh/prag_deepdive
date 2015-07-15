111b99bf4d2d6ca388450f8dd746f8880950dd00
Suyesh Bhandari <suyeshb@gmail.com>
2015-07-15 17:46:52 -0400

    Looping Using numbers

--- deepdive.rb
+++ deepdive.rb
@@ -11,3 +11,10 @@ end
 #Fixnum: 100020001
 #Bignum: 1000400060004001
 #Bignum: 100080028005600700056002800080001
+
+3.times {print "X "}
+1.upto(5) {|i| print i, " "}
+99.downto(95) {|i| print i, " "}
+50.step(80, 5) {|i| print i, " "}
+
+#result X X X 1 2 3 4 5 99 98 97 96 95 50 55 60 65 70 75 80
