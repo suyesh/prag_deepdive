6671cb32ec498884947244f912654c955a42e2c2
Suyesh Bhandari <suyeshb@gmail.com>
2015-07-15 22:54:09 -0400

    More methods

--- method_dive.rb
+++ method_dive.rb
@@ -1,3 +1,12 @@
 puts 1.even? #=> false
 puts 2.even? #=> true
 puts 1.instance_of?(Fixnum) #=> true
+
+def cool_dude(arg1="Miles",arg2="Coltrane",arg3="Roach")
+	puts "#{arg1}, #{arg2}, #{arg3}"
+end
+
+cool_dude
+cool_dude("Matt")
+cool_dude("Matt", "Nischal")
+cool_dude("Matt","Nischal","gavin")
