a822ef0fbdb564865ea85c8dadef682b1474860d
Suyesh Bhandari <suyeshb@gmail.com>
2015-07-15 23:31:58 -0400

    More methods

--- method_block.rb
+++ method_block.rb
@@ -11,3 +11,19 @@ double("tom") do |val|
   puts "Then I got #{val}"
 end
 #then i got tomtom
+
+class TaxCalculator
+  def initialize(name, &block)
+    @name, @block = name, block
+  end
+
+  def get_tax(amount)
+    "#{@name} on #{amount} = #{@block.call(amount)}"
+  end
+
+end
+
+tc = TaxCalculator.new("Sales Tax") {|amt| amt * 0.075}
+
+tc.get_tax(100)
+tc.get_tax(250)
