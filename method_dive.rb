1ec68ff8456c800fec2911792e059524c400eed8
Suyesh Bhandari <suyeshb@gmail.com>
2015-07-15 23:13:12 -0400

    More methods

--- method_dive.rb
+++ method_dive.rb
@@ -10,3 +10,12 @@ cool_dude
 cool_dude("Matt")
 cool_dude("Matt", "Nischal")
 cool_dude("Matt","Nischal","gavin")
+
+
+def surround(word, pad_width= word.length/2)
+	"[" * pad_width + word + "]" * pad_width
+end
+
+puts surround("elephant")
+puts surround("fox")
+puts surround("suyesh", 10)
